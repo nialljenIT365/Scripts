a49b7237c5af5b1e94843578af987641a1607282
nialljenIT365 <39529057+nialljenIT365@users.noreply.github.com>
2025-02-14 11:54:12 +0000

    Add files via upload

---
+++ Unblock-File.ps1
@@ -0,0 +1,6 @@
+﻿Get-ChildItem -Path "C:\Program Files\WindowsPowerShell\Modules\Az.Resources" -Recurse | Unblock-File
+Get-ChildItem -Path "C:\Program Files\WindowsPowerShell\Modules\Az.Compute" -Recurse | Unblock-File
+Get-ChildItem -Path "C:\Program Files\WindowsPowerShell\Modules\Az.KeyVault" -Recurse | Unblock-File
+Get-ChildItem -Path "C:\Program Files\WindowsPowerShell\Modules\Az.Accounts" -Recurse | Unblock-File
+Get-ChildItem -Path "C:\Program Files\WindowsPowerShell\Modules\Az.DesktopVirtualization" -Recurse | Unblock-File
+Get-ChildItem -Path "C:\Program Files\WindowsPowerShell\Modules\Az.Avd" -Recurse | Unblock-File
